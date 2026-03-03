6b3d6f3022f2070960adbdb73ee366a353960b0b
Layos <d.layos@pascualbravo.edu.co>
2026-03-03 12:35:23 +0000

    Procesos aleatorios

--- main.go
+++ main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"gestion-procesos/src/procesos"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
 	fmt.Println("========================================")
 	fmt.Println("  GESTOR DE PROCESOS Y CONCURRENCIA")
-	fmt.Println("========================================\n")
+	fmt.Println("========================================")
 
 	fmt.Println("[COMPONENTE 1] Simulación de Procesos")
 	fmt.Println("----------------------------------------")
 	simulador := procesos.NuevoSimuladorProcesos()
 
-	simulador.CrearProcesoSimulado("Proceso A", 5)
-	simulador.CrearProcesoSimulado("Proceso B", 3)
-	simulador.CrearProcesoSimulado("Proceso C", 7)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 3; i++ {
+
+		min := 1
+		max := 11
+
+		duracion := rand.Intn(max-min+1) + min
+		nombre := fmt.Sprintf("Proceso %d", i)
+
+		simulador.CrearProcesoSimulado(nombre, duracion)
+
+	}
+
+	//simulador.CrearProcesoSimulado("Proceso A", 5)
+	//simulador.CrearProcesoSimulado("Proceso B", 3)
+	//simulador.CrearProcesoSimulado("Proceso C", 7)
 
 	simulador.EjecutarSimulacion()
 	fmt.Println()
